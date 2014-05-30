7063cecc8b4d333b44241c937d430bb107288f78
k29 <kaustubh.nawade@gmail.com>
2014-05-30 12:12:11 +0530

    initial rfid code

---
+++ rfid_read_write.c
@@ -0,0 +1,92 @@
+
+ #include <stdio.h>   /* Standard input/output definitions */
+ #include <string.h>  /* String function definitions */
+ #include <unistd.h>  /* UNIX standard function definitions */
+ #include <fcntl.h>   /* File control definitions */
+ #include <errno.h>   /* Error number definitions */
+#include <termios.h>
+
+int set_interface_attribs (int fd, int speed, int parity)
+{
+        struct termios tty;
+        memset (&tty, 0, sizeof tty);
+        if (tcgetattr (fd, &tty) != 0)
+        {
+                printf ("error %d from tcgetattr", errno);
+                return -1;
+        }
+
+        cfsetospeed (&tty, speed);
+        cfsetispeed (&tty, speed);
+
+        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+        // disable IGNBRK for mismatched speed tests; otherwise receive break
+        // as \000 chars
+        tty.c_iflag &= ~IGNBRK;         // disable break processing
+        tty.c_lflag = 0;                // no signaling chars, no echo,
+                                        // no canonical processing
+        tty.c_oflag = 0;                // no remapping, no delays
+        tty.c_cc[VMIN]  = 0;            // read doesn't block
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+
+        tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
+                                        // enable reading
+        tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
+        tty.c_cflag |= parity;
+        tty.c_cflag &= ~CSTOPB;
+        //tty.c_cflag &= ~CRTSCTS;
+
+        if (tcsetattr (fd, TCSANOW, &tty) != 0)
+        {
+                printf ("error %d from tcsetattr", errno);
+                return -1;
+        }
+        return 0;
+}
+
+void set_blocking (int fd, int should_block)
+{
+        struct termios tty;
+        memset (&tty, 0, sizeof tty);
+        if (tcgetattr (fd, &tty) != 0)
+        {
+                printf ("error %d from tggetattr", errno);
+                return;
+        }
+
+        tty.c_cc[VMIN]  = should_block ? 1 : 0;
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+        if (tcsetattr (fd, TCSANOW, &tty) != 0)
+                printf ("error %d setting term attributes", errno);
+}
+
+
+int main()
+{
+    char *portname = "/dev/ttyS1";
+    int fd = open (portname, O_RDWR | O_NOCTTY | O_SYNC);
+    if (fd < 0)
+    {
+            printf ("error %d opening %s: %s", errno, portname, strerror (errno));
+            return;
+    }
+
+    set_interface_attribs (fd, B115200, 0);  // set speed to 115,200 bps, 8n1 (no parity)
+    set_blocking (fd, 0);                // set no blocking
+
+    write (fd, "0xAABB03010103\n", 15);           // send 7 character greeting
+
+    usleep ((15 + 15) * 100);             // sleep enough to transmit the 7 plus
+                                         // receive 25:  approx 100 uS per char transmit
+    char buf [100];
+    int n = read (fd, buf, sizeof buf);  // read up to 100 characters if ready to read
+    for(int i=0;i<sizeof(buf);i++)
+    {
+        printf("%c\t",buf[i] );
+    }
+    return 0;
+
+}
